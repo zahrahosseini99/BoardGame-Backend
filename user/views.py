41ea9e89b9dbfcd6d7cdeb465efd9be6aa652ab5
Zahra Hosseini <hosseini99.zahra@gmail.com>
2020-10-21 17:59:06 +0330

    [feat] Implement user views

--- user/views.py
+++ user/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
+from .models import UserProfile
+from . import serializers
 
-# Create your views here.
+
+class UserList(generics.ListAPIView):
+    permission_class = (IsAdminUser,)
+    serializer_class = serializers.UserSerializer
+    queryset = UserProfile.objects.all()
+
