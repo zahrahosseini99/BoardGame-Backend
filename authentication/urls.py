62286da655ac4056e07b390c555715a122805831
Zahra Hosseini <hosseini99.zahra@gmail.com>
2020-10-23 14:41:56 +0330

    [feat] Implement authentication/login urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view()),
+    path('login/', views.LoginView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
