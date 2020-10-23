7a267b710a0931dd1e8de28dea4536d866767819
Sepehr <Babapour.Sepehr@gmail.com>
2020-10-23 14:20:03 +0330

    [feat] Implement authentication/login views

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import status
 from user.models import UserProfile
 from . import serializers as auth_serializers
@@ -22,3 +23,19 @@ class RegisterView(generics.CreateAPIView):
         tokens = serializer.data['tokens']
         data = {"refresh": tokens["refresh"], "access": tokens["access"], "id": serializer_user.data["id"]}
         return Response(data, status=status.HTTP_201_CREATED)
+
+
+class LoginView(TokenObtainPairView):
+    """
+        Get Refresh And Access Token by Username and Password Given
+    """
+    serializer_class = auth_serializers.LoginSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        user = UserProfile.objects.get(username=request.data['username'])
+        if serializer.is_valid():
+            serializer.validated_data['id'] = user.id
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
