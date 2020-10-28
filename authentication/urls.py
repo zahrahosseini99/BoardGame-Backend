5929d6a9229cacac95f557284775a1f06ec3dae8
Sepehr <Babapour.Sepehr@gmail.com>
2020-10-28 21:49:24 +0330

    [feat] Implement authentication/edit_profile urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view()),
     path('login/', views.LoginView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('edit_profile/<int:pk>/', views.EditProfileView.as_view(), name='auth_edit_profile'),
     path('change_password/<int:pk>/',views.ChangePasswordView.as_view(), name='auth_change_password'),
 ]
