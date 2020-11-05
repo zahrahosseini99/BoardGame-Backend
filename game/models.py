e77a8eca56a1265abdaa01eb04ea3435b47b3bab
Sepehr <Babapour.Sepehr@gmail.com>
2020-11-06 01:36:31 +0330

    [refactor] Add difficulty to game model and script

--- game/models.py
+++ game/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class game(models.Model):
     name = models.CharField('Name', max_length=200)
     description = models.TextField('Description')
-    category = models.CharField('Category', blank=True, max_length=70)
+    category = models.CharField('Category', blank=True, max_length=200)
     images = models.CharField(blank=True, max_length=2000)
     min_players = models.PositiveIntegerField(null=True)
     max_players = models.PositiveIntegerField(null=True)
+    difficulty = models.FloatField(null=True)
