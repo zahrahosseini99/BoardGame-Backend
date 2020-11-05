86312afa3685f5f440412e127430814331746d51
Sepehr <Babapour.Sepehr@gmail.com>
2020-11-05 23:30:39 +0330

    [feat] Implement game model

--- game/models.py
+++ game/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class game(models.Model):
+    name = models.CharField('Name', max_length=200)
+    description = models.TextField('Description')
+    category = models.CharField('Category', blank=True, max_length=70)
+    images = models.CharField(blank=True, max_length=2000)
+    min_players = models.PositiveIntegerField(null=True)
+    max_players = models.PositiveIntegerField(null=True)
