a109efab54b7516c1c47d25334b43c998d794f74
Sepehr <Babapour.Sepehr@gmail.com>
2020-11-06 17:02:40 +0330

    [feat] Implement game/GamePageInfo views

--- game/views.py
+++ game/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from . import serializers as game_info_page_serializers
+from .models import game
 
-# Create your views here.
+
+class GameInfoPageView(generics.RetrieveAPIView):
+    queryset = game.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = game_info_page_serializers.GameInfoPageSerializer
+
+    def get(self, request, pk=None):
+        gameInfo = game.objects.all().get(pk=pk)
+        serializer = game_info_page_serializers.GameInfoPageSerializer(gameInfo)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
