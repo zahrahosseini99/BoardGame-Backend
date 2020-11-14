eabd49fbdc8cf27ebe74d3f00ba67c5fde258d6d
Sepehr <Babapour.Sepehr@gmail.com>
2020-11-14 14:51:45 +0330

    [feat] Implement play views

--- game/views.py
+++ game/views.py
@@ -1,10 +1,11 @@
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import filters
 from . import serializers as game_info_page_serializers
-from .models import game
+from . import serializers as play_serializer
+from .models import game, play
 from user.models import UserProfile
 from user.serializers import UserSerializer
 
@@ -52,3 +53,22 @@ class SearchGameView(generics.ListAPIView):
     serializer_class = game_info_page_serializers.GameInfoPageSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['^name']
+
+
+class CreatePlayView(generics.CreateAPIView):
+    queryset = play.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = play_serializer.playSerializer
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        data = request.data
+        serializer = self.get_serializer(data=data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        play = serializer.save()
+        play.players.add(user)
+        for player_data in data['players']:
+            player = UserProfile.objects.all().get(username=player_data['username'])
+            play.players.add(player)
+        return Response("OK", status=status.HTTP_202_ACCEPTED)
