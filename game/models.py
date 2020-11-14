cbf1a3cd12fe78e3fecae6cfe07969304aad95ec
Zahra Hosseini <hosseini99.zahra@gmail.com>
2020-11-14 16:00:14 +0330

    [fix] Update play model

--- game/models.py
+++ game/models.py
@@ -16,7 +16,7 @@ class game(models.Model):
 
 class play(models.Model):
     id = models.AutoField(primary_key=True)
-    players = models.ManyToManyField(UserProfile, related_name='play', null=True)
+    players = models.ManyToManyField(UserProfile, related_name='play', blank=True)
     game = models.ForeignKey(game, related_name='play', on_delete=models.CASCADE)
     date = models.DateField()
     place = models.CharField(blank=True, max_length=2000)
