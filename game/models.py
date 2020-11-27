e425619dd2cb91a0b4995695ecec927a989af2c3
Sepehr <Babapour.Sepehr@gmail.com>
2020-11-27 10:19:10 +0330

    [feat] Update play model and add playmate model

--- game/models.py
+++ game/models.py
@@ -14,19 +14,20 @@ class game(models.Model):
     rate = models.FloatField(null=True)
 
 
+class playmate(models.Model):
+    username = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    color = models.CharField('Color', max_length=200, blank=True)
+    starting_position = models.CharField('Starting position', max_length=200, blank=True)
+    score = models.CharField('Score', max_length=200, blank=True)
+    is_won = models.BooleanField('Is won', default=False)
+    is_first_time = models.BooleanField('Is first time', default=False)
+
+
 class play(models.Model):
     id = models.AutoField(primary_key=True)
-    owner=models.ForeignKey(UserProfile,related_name='owner',null=True,  on_delete=models.CASCADE)
-    players = models.ManyToManyField(UserProfile, related_name='play', blank=True,through='playmate')
+    owner = models.ForeignKey(UserProfile, related_name='owner', null=True, on_delete=models.CASCADE)
+    players = models.ManyToManyField(playmate, related_name='play', blank=True)
+    semi_players = models.CharField(blank=True, max_length=2000)
     game = models.ForeignKey(game, related_name='play', on_delete=models.CASCADE)
     date = models.DateField()
     place = models.CharField(blank=True, max_length=2000)
-
-class playmate(models.Model):
-    play=models.ForeignKey(play,on_delete=models.CASCADE)
-    user=models.ForeignKey(UserProfile,on_delete=models.CASCADE)
-    color=models.CharField('Color', max_length=200, blank=True)
-    starting_position=models.CharField('Starting position', max_length=200, blank=True)
-    score=models.CharField('Score', max_length=200, blank=True)
-    is_won=models.BooleanField('Is won', default=False)
-    is_first_time=models.BooleanField('Is first time', default=False)
