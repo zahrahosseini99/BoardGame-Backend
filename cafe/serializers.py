560dbc11e9f6dee03fde36fe382dd19c8e3a0b3d
Zahra Hosseini <hosseini99.zahra@gmail.com>
2020-11-27 12:15:14 +0330

    [feat] Implement cafe serializers

---
+++ cafe/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from . import models
+from user.models import UserProfile
+
+
+
+class GameCafeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.game
+        fields = ('id', )
+
+
+class CafeOwnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ('id', )
+
+
+class CafeSerializer(serializers.ModelSerializer):
+    games = GameCafeSerializer(many=True, read_only=True, required=False)
+    owner = CafeOwnerSerializer
+
+    class Meta:
+        model = models.Cafe
+        fields = ('id', 'name', 'owner', 'description', 'games', 'price', 'open_time', 'close_time', 'phone_number', 'gallery', 'city', 'latitude', 'longitude')
+        read_only_fields = ('games', )
+
