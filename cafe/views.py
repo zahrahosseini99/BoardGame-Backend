6f0c211093625f7878a8d1870d6fec808b8f7717
Zahra Hosseini <hosseini99.zahra@gmail.com>
2020-11-27 12:22:48 +0330

    [feat] Implement search cafe view

--- cafe/views.py
+++ cafe/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import filters
 from . import serializers as cafe_serializer
 from .models import Cafe
 from user.models import UserProfile
 from game.models import game
+
+
+class SearchCafeView(generics.ListAPIView):
+    queryset = Cafe.objects.all()
+    serializer_class = cafe_serializer.CafeSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['^name']
 
 
 class CreateCafeView(generics.CreateAPIView):
