d37353da9b74da6bd53315d2f9bad74979fa10d7
Zahra Hosseini <hosseini99.zahra@gmail.com>
2020-11-27 12:20:04 +0330

    [feat] Implement create cafe view

--- cafe/views.py
+++ cafe/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from . import serializers as cafe_serializer
+from .models import Cafe
+from user.models import UserProfile
+from game.models import game
 
-# Create your views here.
+
+class CreateCafeView(generics.CreateAPIView):
+    queryset = Cafe.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = cafe_serializer.CafeSerializer
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        data = request.data
+        data['owner'] = user.id
+        serializer = self.get_serializer(data=data)
+        if not serializer.is_valid(True):
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        cafe = serializer.save()
+        for game_id in data['games']:
+            g = game.objects.get(id=game_id['id'])
+            cafe.games.add(g)
+        return Response("OK", status=status.HTTP_202_ACCEPTED)
