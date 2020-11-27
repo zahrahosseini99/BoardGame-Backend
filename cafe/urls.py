95dcb67adc945cdc539810199f932a0b39343a37
Sepehr <Babapour.Sepehr@gmail.com>
2020-11-27 12:31:22 +0330

    [feat] Add owner cafe list urls

--- cafe/urls.py
+++ cafe/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('create_cafe/', views.CreateCafeView.as_view()),
     path('search_cafe/name/', views.SearchCafeView.as_view()),
+    path('owner_cafes_list/', views.OwnerCafesListView.as_view()),
 ]
