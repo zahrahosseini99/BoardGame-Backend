b31529aa7e7a059aaf8dab13e81d6b1a4677e038
Zahra Hosseini <hosseini99.zahra@gmail.com>
2020-12-11 19:37:58 +0330

    [feat] Implement community models

--- community/models.py
+++ community/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from user.models import UserProfile
+from cafe.models import Gallery
 
 # Create your models here.
+
+class Community(models.Model):
+    name = models.CharField(blank=True, max_length=2000)
+    owner = models.ForeignKey(UserProfile, related_name='owner', null=True, on_delete=models.CASCADE)
+    members = models.ManyToManyField(UserProfile, related_name='Community', blank=True)
+    description = models.TextField('Description')
+    image = models.ForeignKey(Gallery, related_name='Community', blank=True, on_delete=models.CASCADE)
+    lock = models.BooleanField('Lock', default=False)
+   
