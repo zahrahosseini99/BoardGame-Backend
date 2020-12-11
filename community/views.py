25a94dd3490d0764c9cfea5419c4f408d2b95df7
Sepehr <Babapour.Sepehr@gmail.com>
2020-12-11 19:57:52 +0330

    [feat] Implement create community view

--- community/views.py
+++ community/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from . import serializers as community_serializer
+from .models import Community
+from user.models import UserProfile
 
-# Create your views here.
+
+class CreateCommunityView(generics.CreateAPIView):
+    queryset = Community.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = community_serializer.CommunitySerializer
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        data = request.data
+        for member in data['members']:
+            member['username'] = UserProfile.objects.get(username=member['username']).id
+        data['owner'] = user.id
+        serializer = self.get_serializer(data=data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save()
+        return Response("OK", status=status.HTTP_202_ACCEPTED)
