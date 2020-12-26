f460bc4fc54345bc94cf9e0cad9a314ad9a3f2f4
Zahra Hosseini <hosseini99.zahra@gmail.com>
2020-12-26 20:01:29 +0330

    [feat] Implement event model

--- community/models.py
+++ community/models.py
@@ -10,3 +10,14 @@ class Community(models.Model):
     description = models.TextField('Description')
     image = models.ForeignKey(Gallery, related_name='community', null=True, blank=True, on_delete=models.CASCADE)
     lock = models.BooleanField('Lock', default=False)
+
+class Event(models.Model):
+    owner = models.ForeignKey(UserProfile, related_name='Event_owner', null=True, on_delete=models.CASCADE)
+    members = models.ManyToManyField(UserProfile, related_name='Event_member', blank=True)
+    games = models.ManyToManyField(game, related_name='Event', blank=True)
+    date = models.DateField()
+    time = models.TimeField()
+    maxMember = models.PositiveIntegerField()
+    place = models.CharField(blank=True, max_length=2000)
+    gallery = models.ManyToManyField(Gallery, related_name='Event', blank=True)
+    plays = models.ManyToManyField(play, related_name='Event', blank=True)
