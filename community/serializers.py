e9e5c00d6e2c86a337d854a51c5e5a2009670fdf
Zahra Hosseini <hosseini99.zahra@gmail.com>
2020-12-31 17:00:31 +0330

    [feat] Implement event serializer

--- community/serializers.py
+++ community/serializers.py
@@ -4,6 +4,43 @@ from user.serializers import UserSerializer, FullUserSerializer
 from cafe.serializers import CafeGallerySerializer, GameCafeSerializer
 from game.models import play, game
 
+class FullPlaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.play
+        fields = ('id', 'players', 'semi_players', 'game', 'date', 'place', 'owner')
+
+
+class EventSerializer(serializers.ModelSerializer):
+    owner = UserSerializer
+    members = FullUserSerializer(many=True, read_only=True, required=False)
+    games = GameCafeSerializer(many=True, read_only=True, required=False)
+    gallery = CafeGallerySerializer(read_only=True)
+    plays = FullPlaySerializer(read_only=True)
+
+    class Meta:
+        model = models.Event
+        fields = ('owner', 'members', 'games', 'date', 'time', 'maxMember', 'place', 'gallery', 'plays')
+        read_only_fields = ('members', 'games', 'gallery', 'plays')
+
+
+class MinusPlaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.play
+        fields = ('id',)
+
+        
+class editEventSerializer(serializers.ModelSerializer):
+    owner = UserSerializer
+    members = FullUserSerializer(many=True, read_only=True, required=False)
+    games = GameCafeSerializer(many=True, read_only=True, required=False)
+    gallery = CafeGallerySerializer(many=True, read_only=True, required=False)
+    plays = MinusPlaySerializer(many=True, read_only=True, required=False)
+
+    class Meta:
+        model = models.Event
+        fields = ('owner', 'members', 'games', 'date', 'time', 'maxMember', 'place', 'gallery', 'plays')
+        read_only_fields = ('members', 'games', 'gallery', 'plays')
+
 
 class CommunitySerializer(serializers.ModelSerializer):
     owner = UserSerializer
@@ -27,20 +64,3 @@ class CommunitiesListSerializer(serializers.ModelSerializer):
         read_only_fields = ('members', 'image')
 
 
-class FullPlaySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.play
-        fields = ('id', 'players', 'semi_players', 'game', 'date', 'place', 'owner')
-
-
-class EventSerializer(serializers.ModelSerializer):
-    owner = UserSerializer
-    members = FullUserSerializer(many=True, read_only=True, required=False)
-    games = GameCafeSerializer(many=True, read_only=True, required=False)
-    gallery = CafeGallerySerializer(read_only=True)
-    plays = FullPlaySerializer(read_only=True)
-
-    class Meta:
-        model = models.Event
-        fields = ('owner', 'members', 'games', 'date', 'time', 'maxMember', 'place', 'gallery', 'plays')
-        read_only_fields = ('members', 'games', 'gallery', 'plays')
